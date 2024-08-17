882fd5fde35494bdeac379b69c5da85112cdbe05
m-nirupama <146970289+m-nirupama@users.noreply.github.com>
2024-08-17 15:55:51 +0530

    Create Sum of Sums of Digits in Cyclic order.java

---
+++ Sum of Sums of Digits in Cyclic order.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class SumOfSumsOfDigitsInCyclicOrderV1 {
+    public int sumOfSumsOfDigits(int input1) {
+        String num = String.valueOf(input1);
+        int sum = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            int tempSum = 0;
+            for (int j = i; j < num.length(); j++) {
+                tempSum += Character.getNumericValue(num.charAt(j));
+            }
+            sum += tempSum;
+        }
+        return sum;
+    }
+}
