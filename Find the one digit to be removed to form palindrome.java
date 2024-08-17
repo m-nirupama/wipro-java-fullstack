c100886a47eec014e1fd0c3cb294a2cd89895def
m-nirupama <146970289+m-nirupama@users.noreply.github.com>
2024-08-17 16:23:12 +0530

    Create Find the one digit to be removed to form palindrome.java

---
+++ Find the one digit to be removed to form palindrome.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Remove1DigitForPalindromeV1 {
+    public int digitRemove_Palin(int input1) {
+        String numStr = String.valueOf(input1);
+
+        for (int i = 0; i < numStr.length(); i++) {
+            String newNum = numStr.substring(0, i) + numStr.substring(i + 1);
+            if (isPalindrome(newNum)) {
+                return Character.getNumericValue(numStr.charAt(i));
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
