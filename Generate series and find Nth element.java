5b011139c96f3480372a8523dbf2b331174c5336
m-nirupama <146970289+m-nirupama@users.noreply.github.com>
2024-08-18 10:30:00 +0530

    Update Generate series and find Nth element.java

--- Generate series and find Nth element.java
+++ Generate series and find Nth element.java
@@ -1,4 +1,4 @@
-public class GenerateSeriesFindNV1 {
+public class GenerateSeriesFindNV {
     public int generateSeriesFindN(int input1, int input2, int input3, int input4) {
         int gap1 = input2 - input1;
         int gap2 = input3 - input2;
