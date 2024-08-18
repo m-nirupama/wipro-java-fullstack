847ade6ad05f30a065b3eb52a6fd8959d7c9dca1
m-nirupama <146970289+m-nirupama@users.noreply.github.com>
2024-08-18 10:27:37 +0530

    Update AdditionUsingStrings.java

--- AdditionUsingStrings.java
+++ AdditionUsingStrings.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 import java.math.BigInteger;
 
-class AdditionUsingStringsV1 {
+class AdditionUsingStringsV {
     public String additionUsingStrings(String input1, String input2) {
         BigInteger x = new BigInteger(input1);
         BigInteger y = new BigInteger(input2);
