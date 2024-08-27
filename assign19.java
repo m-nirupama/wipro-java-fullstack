5281dccbb2c7b58a81102d0159f104c2bbf0f9bf
m-nirupama <146970289+m-nirupama@users.noreply.github.com>
2024-08-27 10:16:16 +0530

    assign19.java

---
+++ assign19.java
@@ -0,0 +1,16 @@
+package myPack1;
+
+public class assign19 {
+	    public static void main(String[] args) {
+	        int count = 0;
+	        int num = 2;
+	        while (count != 5) {
+	            if (num % 2 == 0 && num % 3 == 0 && num % 5 == 0) {
+	                count++;
+	                System.out.println(num);
+	            }
+	            num++;
+	        }
+	    }
+	}
+
